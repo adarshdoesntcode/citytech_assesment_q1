e056181fc4549986b868f9bb46d3fe42786f9406
Adarsh Das <adarsh.191605@ncit.edu.np>
2025-03-21 16:11:55 +0545

    Add 'i < j' condition in inner loop

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const countInversions = (arr) => {
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
+      if (i < j && arr[i] > arr[j]) {
         count++;
       }
     }
